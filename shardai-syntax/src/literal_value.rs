bf8397ae5cdf3a4ecd4d722b3222f814b3ab2d9e
wyteroze <halo@hp4.lol>
2026-03-05 02:17:27 -0500

    Remove crate that was never committed

--- shardai-syntax/src/literal_value.rs
+++ shardai-syntax/src/literal_value.rs
@@ -1,22 +1,9 @@
 // Copyright 2026 wyteroze. Licensed under the Apache License, Version 2.0.
 
-use shardai_bytecode::constant::Constant;
-
 #[derive(Clone, Debug)]
 pub enum LiteralValue {
     Number(f64),
     String(String),
     Bool(bool),
     Nil,
-}
-
-impl From<Constant> for LiteralValue {
-    fn from(value: Constant) -> Self {
-        match value {
-            Constant::Nil => LiteralValue::Nil,
-            Constant::Boolean(b) => LiteralValue::Bool(b),
-            Constant::Number(n) => LiteralValue::Number(n),
-            Constant::String(s) => LiteralValue::String(s),
-        }
-    }
 }
