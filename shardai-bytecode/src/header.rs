c708afed1e7c414e3039c558c852761709bb2038
wyteroze <halo@hp4.lol>
2026-03-10 15:51:38 -0400

    Implement BytecodeHeader struct, has signature, version, constant count

---
+++ shardai-bytecode/src/header.rs
@@ -0,0 +1,23 @@
+// Copyright 2026 wyteroze. Licensed under the Apache License, Version 2.0.
+
+use std::fmt::{Debug, Formatter};
+
+pub struct BytecodeHeader {
+    pub signature: [u8; 3], // "SBC" (Shardai bytecode)
+    pub version_major: u8,
+    pub version_minor: u8,
+    pub constant_count: u32
+}
+
+impl Debug for BytecodeHeader {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let signature = str::from_utf8(&self.signature)
+            .unwrap_or_else(|_| "Invalid UTF8 signature");
+
+        write!(f, "Signature: {},", signature)?;
+        write!(f, " Version: {}.{},", self.version_major, self.version_minor)?;
+        write!(f, " Constant Count: {}", self.constant_count)?;
+
+        Ok(())
+    }
+}
