4feceebca43a4e2e99752e96b4679490ed2669d3
wyteroze <me@wyteroze.net>
2026-04-24 17:36:46 -0400

    formatting changes for jump_if_truthy()

--- shardai-vm/src/vm.rs
+++ shardai-vm/src/vm.rs
@@ -230,7 +230,6 @@ impl VM {
     #[inline]
     fn jump_if_truthy(&mut self, a: u8, b: u8, c: u8) -> Result<(), RuntimeError> {
         let value = self.registers[c as usize];
-
         if self.is_truthy(&value) {
             self.jump(a, b)?
         }
