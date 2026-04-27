a7627e8c06c9cd8cff7a9d81abd0f59e95858221
wyteroze <me@wyteroze.net>
2026-04-27 15:28:40 -0400

    add RegisterAllocator

--- shardai-compiler/src/compiler.rs
+++ shardai-compiler/src/compiler.rs
@@ -19,8 +19,62 @@ enum Local {
     Immutable(u8),
 }
 
-pub struct Compiler {
-    next_register: u8, // This sucks really badly since we can't free registers but we'll fix it later
+#[derive(Clone)]
+struct RegisterAllocator {
+    next_register: u8,
+    freed_registers: Vec<u8>,
+    scopes: Vec<u8>
+}
+
+impl RegisterAllocator {
+    pub fn new() -> Self {
+        Self {
+            next_register: 0,
+            freed_registers: Vec::new(),
+            scopes: Vec::new(),
+        }
+    }
+
+    pub fn alloc(&mut self) -> u8 {
+        if let Some(reg) = self.freed_registers.pop() {
+            reg
+        } else {
+            let reg = self.next_register;
+            self.next_register = self.next_register.checked_add(1)
+                .expect("register overflow");
+
+            reg
+        }
+    }
+
+    pub fn dealloc(&mut self, register: u8) {
+        if self.next_register - 1 == register {
+            self.next_register -= 1;
+            self.freed_registers.retain(|r| *r < self.next_register);
+        } else {
+            self.freed_registers.push(register)
+        }
+    }
+
+    pub fn push_scope(&mut self) {
+        self.scopes.push(self.next_register)
+    }
+
+    pub fn pop_scope(&mut self) {
+        let old = self.scopes.pop()
+            .expect("no scope to pop");
+
+        self.freed_registers.retain(|r| *r < old);
+        self.next_register = old;
+    }
+
+    pub fn frame_size(&self) -> u8 {
+        self.next_register
+    }
+}
+
+#[derive(Clone)]
+struct CompilerFrame {
     locals: HashMap<String, Local>,
     constants: Vec<Constant>,
     instructions: Vec<Instruction>,
