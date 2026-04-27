1ec8ace6608a408c18bae6d06db1914d7683a18e
wyteroze <me@wyteroze.net>
2026-04-27 15:31:52 -0400

    implement frame_mut() function

--- shardai-compiler/src/compiler.rs
+++ shardai-compiler/src/compiler.rs
@@ -113,6 +113,10 @@ impl Compiler {
     fn frame(&self) -> &CompilerFrame {
         self.compiler_frames.last().unwrap()
     }
+
+    fn frame_mut(&mut self) -> &mut CompilerFrame {
+        self.compiler_frames.last_mut().unwrap()
+    }
     fn add_constant(&mut self, constant: Constant) -> Result<u8, CompileError> {
         if self.constants.len() >= u8::MAX as usize {
             return Err(CompileError::TooManyConstants);
