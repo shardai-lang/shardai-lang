22be26c4ed274885cc9e84bc3fa9e8104233efb2
wyteroze <me@wyteroze.net>
2026-04-28 23:39:51 -0400

    update multiply() to use callframe system

--- shardai-vm/src/vm.rs
+++ shardai-vm/src/vm.rs
@@ -211,13 +211,13 @@ impl VM {
 
     #[inline]
     fn multiply(&mut self, a: u8, b: u8, c: u8) -> Result<(), RuntimeError> {
-        let left = self.registers[b as usize];
-        let right = self.registers[c as usize];
+        let left = self.get_register(b);
+        let right = self.get_register(c);
 
         if let Value::Number(l) = left
             && let Value::Number(r) = right
         {
-            self.registers[a as usize] = Value::Number(l * r);
+            self.set_register(a, Value::Number(l * r));
             return Ok(());
         }
 
