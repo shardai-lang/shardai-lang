f9eb05651708dce4bf45754c90fbf1dd273db5e1
wyteroze <me@wyteroze.net>
2026-04-28 23:40:50 -0400

    update modulo() to use callframe system

--- shardai-vm/src/vm.rs
+++ shardai-vm/src/vm.rs
@@ -276,13 +276,13 @@ impl VM {
 
     #[inline]
     fn modulo(&mut self, a: u8, b: u8, c: u8) -> Result<(), RuntimeError> {
-        let left = self.registers[b as usize];
-        let right = self.registers[c as usize];
+        let left = self.get_register(b);
+        let right = self.get_register(c);
 
         if let Value::Number(l) = left
             && let Value::Number(r) = right
         {
-            self.registers[a as usize] = Value::Number(l.rem_euclid(r));
+            self.set_register(a, Value::Number(l.rem_euclid(r)));
             return Ok(());
         }
 
